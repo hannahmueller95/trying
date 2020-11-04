8a573558cfbf4dce0883ec802191de86b28d5626
hannahmueller95 <72393046+hannahmueller95@users.noreply.github.com>
2020-11-04 13:31:22 +0100

    Add files via upload

---
+++ km-m.py
@@ -0,0 +1,8 @@
+user_name = input('Put name here: ');
+print('Hello ' + user_name + '!')
+
+
+kilometer = int(input('Enter kilometers (whole numbers) to convert in miles: '))
+miles = kilometer * 0.621371
+print('{0} kilometers are {1} miles'.format(kilometer, miles))
+
